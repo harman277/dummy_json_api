341b96902cc2c686d386984d8ec3469faadd663f
harman277 <hs4993681@gmail.com>
2025-03-23 22:02:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,5 +21,9 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use("/api/dummy", dummyRoutes);
 
+app.get('/',async(_,res)=>{
+    res.send(<h1>Working fine</h1>)
+})
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
