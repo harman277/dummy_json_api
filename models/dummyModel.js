cf64678757594e005acb11e960afb7a35ff95f15
harman277 <hs4993681@gmail.com>
2025-03-23 23:53:28 +0530

    Update dummyModel.js

--- models/dummyModel.js
+++ models/dummyModel.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const DummySchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    age: { type: Number, required: true },
-    phone: { type: String, required: true, unique: true },
-    address: { type: String, required: true }
+  name: { type: String, required: true, unique: true },
+    species: { type: String, required: true }, 
+    age: { type: Number, required: true }, 
+    habitat: { type: String, required: true },
+    diet: { type: String, required: true }, 
+    conservationStatus: { type: String, required: true }, 
 });
 
 module.exports = mongoose.model("Dummy", DummySchema);
